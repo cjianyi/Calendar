f5672e23f5b85a6cdbb2d2c4373dafbbc2e51ef4
Shilong <shilong.yu@mail.utoronto.ca>
2020-03-03 15:40:41 -0500

    Initial skeletons

--- phase1/calendarProject/src/Controller.java
+++ phase1/calendarProject/src/Controller.java
@@ -1,2 +1,17 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.Scanner;
 public class Controller {
+    public Controller() {
+
+    }
+    public void getInput() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Username: ");
+        String s1 = in.nextLine();
+        System.out.print("Password: ");
+        String s2 = in.nextLine();
+        System.out.println(s1);
+        System.out.println(s2);
+    }
 }
