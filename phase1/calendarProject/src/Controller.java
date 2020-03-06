3972e61bf20e45d8a472a442b81f473c1e9ce47f
hallmat5 <matthew.hall@mail.utoronto.ca>
2020-03-06 16:07:51 -0500

    menues now fully functional

--- phase1/calendarProject/src/Controller.java
+++ phase1/calendarProject/src/Controller.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Scanner;
 import java.util.Stack;
-
+import java.util.ArrayList;
 public class Controller {
     Scanner in;
     static java.lang.reflect.Method method;
@@ -25,49 +25,143 @@ public class Controller {
         menuStack.push("mainMenu");
     }
 
-    public void displayMenu() {
+    public void displayMenu() throws IOException {
         while (true) {
-            try {
-                method = this.getClass().getMethod(menuStack.peek());
-            } catch (SecurityException e) {
-            } catch (NoSuchMethodException e) {
-            }
-            ;
+            switch (menuStack.peek()) {
+                case "mainMenu":
+                    mainMenu();
+                    break;
+                case "logInMenu":
+                    logInMenu();
+                    break;
+                case "accountGetter":
+                    accountGetter();
+                    menuStack = new Stack<String>();
+                    menuStack.push("mainMenu");
+                    break;
+                case "calendarMenu":
+                    calendarMenu();
+                    break;
+                case "editorMenu":
+                    editorMenu();
+                    menuStack.pop();
+                    break;
+                case "eventMenu":
+                    eventMenu();
 
-            try {
-                method.invoke(this);
-            } catch (IllegalArgumentException e) {
-            } catch (IllegalAccessException e) {
-            } catch (InvocationTargetException e) {
+                    break;
+                case "searchMenu":
+                    searchMenu();
+                    menuStack.pop();
+                    break;
+                case "createEventMenu":
+                    createEventMenu();
+                    menuStack.pop();
+                    break;
+                case "deleteEventMenu":
+                    deleteEventMenu();
+                    menuStack.pop();
+                    break;
+                case "editEventMenu":
+                    editEventMenu();
+                    menuStack.pop();
+                    break;
+                case "linkEventMenu":
+                    linkEventMenu();
+                    menuStack.pop();
+                    break;
             }
-
-            System.out.println("tsrhtrsh");
         }
     }
 
     public void calendarMenu(){
-        System.out.println("Press 1 to open event editor\n Press 2 to open to events");
+        System.out.println("\nCalendar Menu\nPress 1 to open event editor\n Press 2 to open to events");
         String choice = this.in.nextLine();
         if(choice.equals("1")){
-            editorMenu();
+            menuStack.push("editorMenu");
         }else if(choice.equals("2")){
-            eventMenu();
+            menuStack.push("eventMenu");
         }
     }
 
     public void editorMenu(){
-        System.out.println("Editor Menu\n Press 1 to create an event\nPress 2 to delete an event" +
-                "\nPress 3 to edit an event");
+        System.out.println("\nEditor Menu\n Press 1 to create an event\nPress 2 to delete an event" +
+                "\nPress 3 to edit an event\nPress 4 to link events");
+        String choice = this.in.nextLine();
+        switch (choice) {
+            case "1":
+                menuStack.push("createEventMenu");
+                break;
+            case "2":
+                menuStack.push("deleteEventMenu");
+                break;
+            case "3":
+                menuStack.push("editEventMenu");
+                break;
+            case "4":
+                menuStack.push("linkEventMenu");
+                break;
+        }
+
+    }
+
+    public void createEventMenu(){
+        //date, time, tag, memo, seriesame, alert, freq, duration
+    }
+
+    public void deleteEventMenu(){
+
+    }
+
+    public void editEventMenu(){
+
+    }
+
+    public void linkEventMenu(){
+
     }
 
     public void eventMenu(){
-        System.out.println("Event menu\n Press 1 to view past event\nPress 2 to view current events" +
+        System.out.println("\nEvent menu\n Press 1 to view past event\nPress 2 to view current events" +
                 "\nPress 3 to view future event\nPress 4 to view all events\nPress 5 open search menu");
+        String choice = this.in.nextLine();
+        switch(choice){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                menuStack.push("searchMenu");
+                break;
+        }
     }
 
     public void searchMenu(){
-        System.out.println("Search menu\n Press 1 to search by day\nPress to search by tag\n" +
-                "Press 3 to search by series name\n Press to search events by name");
+        System.out.println("\nSearch menu\n Press 1 to search by day\nPress to search by tag\n" +
+                "Press 3 to search by series name\n Press 4 to search events by name");
+        String choice = this.in.nextLine();
+        switch(choice){
+            case "1":
+                System.out.println("Enter the day you want to search");
+                choice = this.in.nextLine();
+                ArrayList<Event> events = currentCalendar.search();
+                break;
+            case "2":
+                System.out.println("Enter the tag you want to search");
+                choice = this.in.nextLine();
+                break;
+            case "3":
+                System.out.println("Enter the series name you want to search");
+                choice = this.in.nextLine();
+                break;
+        }
+
+        System.out.println("\nPress any key to continue");
+        choice = this.in.nextLine();
     }
 
     private void accountGetter() throws IOException {
@@ -101,9 +195,7 @@ public class Controller {
     }
 
     public void mainMenu() throws IOException {
-        exit = false;
-        while(!exit) {
-            System.out.println("Enter 1 to log in, 2 to create new account, -1 to exit");
+            System.out.println("\nMain Menu\nEnter 1 to log in, 2 to create new account, -1 to exit");
             String log = this.in.nextLine();
             if (log.equals("1")) {
                 menuStack.push("logInMenu");
@@ -112,41 +204,30 @@ public class Controller {
             }else if (log.equals("-1")){
                 exit = true;
             }
-        }
-        if(loggedIn){
-            calendarMenu();
-        }
     }
 
-    private void logInGetter() throws IOException {
-        exit = false;
-        System.out.println("Enter your username or email, or enter -1 to go back to the main menu");
-        username = this.in.nextLine();
-        if(username.equals("-1")){
-            mainMenu();
-            exit = true;
-        }
-        if(!exit) {
-            System.out.println("Enter your password");
-            password = this.in.nextLine();
-        }
-    }
 
     private void logInMenu() throws IOException {
-        exit = false;
-        while(!exit) {
-            logInGetter();
-            if (!exit) {
-                User user = this.userManager.logIn(username, password);
-                if (user != null) {
-                    System.out.println("Log in success!");
-                    this.currentUser = user;
-                    loggedIn = true;
-                    exit = true;
-                } else {
-                    System.out.println("The username or password you entered is incorrect");
-                }
+        System.out.println("\nLogin Menu\nEnter your username or email, or enter -1 to go back to the main menu");
+        username = this.in.nextLine();
+        if (username.equals("-1")) {
+            menuStack = new Stack<String>();
+            menuStack.push("mainMenu");
+        } else {
+
+            System.out.println("Enter your password");
+            password = this.in.nextLine();
+
+            User user = this.userManager.logIn(username, password);
+            if (user != null) {
+                System.out.println("Log in success!");
+                this.currentUser = user;
+                menuStack.pop();
+                menuStack.push("calendarMenu");
+            } else {
+                System.out.println("The username or password you entered is incorrect");
             }
         }
     }
+
 }
