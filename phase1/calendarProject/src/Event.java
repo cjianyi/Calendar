819b4f74af5affe4322e51a9156ea8a865b6c868
hallmat5 <matthew.hall@mail.utoronto.ca>
2020-03-06 16:30:19 -0500

    event class

--- phase1/calendarProject/src/Event.java
+++ phase1/calendarProject/src/Event.java
@@ -1,3 +1,18 @@
+import java.util.Date;
 public class Event {
+    private String tag;
+    private Date date;
+    private String name;
+    private String serieName;
+
+    public String getTag(){
+        return this.tag;
+    }
+
+    public Date getDate(){
+        return this.date;
+    }
+
+
 
 }
