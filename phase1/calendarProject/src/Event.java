c6606f579770385c1734250033b8ee6d3e593617
Tony Shi <tonyuniversity.shi@mail.utoronto.ca>
2020-03-06 19:28:45 -0500

    added getters to event

--- phase1/calendarProject/src/Event.java
+++ phase1/calendarProject/src/Event.java
@@ -18,11 +18,29 @@ public class Event implements Comparable<Event>{
         alerts = new ArrayList<Alert>();
         series = new ArrayList<Series>();
     }
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+
     public String getName() {
         return  this.name;
     }
     public void setName(String name) {
         this.name = name;
+    }
+
+    public ArrayList<String> getTags() {
+        return new ArrayList<String>(this.tags);
     }
     public void addTag(String tag) {
         this.tags.add(tag);
@@ -30,12 +48,20 @@ public class Event implements Comparable<Event>{
     public void removeTag(String tag) {
         this.tags.remove(tag);
     }
+
+    public ArrayList<Alert> getAlerts() {
+        return new ArrayList<Alert>(this.alerts);
+    }
     public void addAlert(Alert alert) {
         this.alerts.add(alert);
         // point alert to this event
     }
     public void removeAlert(Alert alert) {
         this.alerts.remove(alert);
+    }
+
+    public ArrayList<Series> getSeries() {
+        return new ArrayList<Series>(this.series);
     }
     public void addSeries(Series series) {
         this.series.add(series);
