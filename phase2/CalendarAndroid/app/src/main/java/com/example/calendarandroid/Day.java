d7cc243b37159e7eca97214dd90ceee7db5efadd
hallmat5 <matthew.hall@mail.utoronto.ca>
2020-03-30 14:55:54 -0400

    added files

---
+++ phase2/CalendarAndroid/app/src/main/java/com/example/calendarandroid/Day.java
@@ -0,0 +1,42 @@
+package com.example.calendarandroid;
+
+import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Collections;
+
+public class Day implements Comparable<Day>{
+    private LocalDate date;
+    private ArrayList<Event> events;
+    private ArrayList<Alert> alerts;
+
+    public Day(LocalDate date) {
+        this.date = date;
+    }
+
+    public void addEvent(Event event) {
+        this.events.add(event);
+        Collections.sort(this.events);
+    }
+    public void removeEvent(Event event) {
+        this.events.remove(event);
+    }
+    public void addAlert(Alert alert) {
+        this.alerts.add(alert);
+    }
+    public void removeAlert(Alert alert) {
+        this.alerts.remove(alert);
+    }
+
+    public ArrayList<Event> getEvents() {
+        return events;
+    }
+
+    public ArrayList<Alert> getAlerts() {
+        return alerts;
+    }
+
+    @Override
+    public int compareTo(Day other) {
+        return this.date.compareTo(other.date);
+    }
+}
