1fd10fa6aed6746c5438864e046cd14957205ac7
hongche4 <chenhao.hong@utoronto.ca>
2020-04-02 20:40:44 -0400

    add the GUI for editing the event

---
+++ phase2/CalendarAndroid/app/src/main/java/com/example/calendarandroid/EditeventActivity.java
@@ -0,0 +1,78 @@
+package com.example.calendarandroid;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class EditeventActivity extends AppCompatActivity{
+    private  EditText event_name;
+    private Event e;
+    private TextView event_message;
+    private Button back_b;
+    private Button event_b;
+    private Button tag_b;
+    private Button alert_b;
+    private Button series_b;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_event);
+        event_name = findViewById(R.id.editeventname);
+        event_message = findViewById(R.id.event_message);
+        back_b = findViewById(R.id.button);
+        event_b = findViewById(R.id.button2);
+        tag_b = findViewById(R.id.button3);
+        alert_b = findViewById(R.id.button4);
+        series_b = findViewById(R.id.button5);
+
+
+    }
+    TextWatcher name = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (matched_name()){
+                event_message.setText(R.string.wrong_event_name);
+            }else{
+                event_message.setText(R.string.correct_event_name);
+            }
+        }
+    };
+
+    public boolean matched_name(){
+        String name= event_name.getText().toString();
+        boolean flag = false;
+        for (Event e : MainActivity.currentCalendar.getEvents()){
+            if (e.getName().equals(event_name)) {
+                this.e = e;
+                flag = true;
+                break;
+            }
+        }
+        if (flag == false) {
+            this.e = null;
+        }
+        return (this.e.equals(null));
+    }
+
+
+
+}
