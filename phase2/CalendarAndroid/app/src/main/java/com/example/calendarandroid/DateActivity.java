400e53815bb8b9a65d9eba66df81300fb59bda66
Arvin <arvinz2k@gmail.com>
2020-04-14 15:04:28 -0700

    Date activity functionality added

--- phase2/CalendarAndroid/app/src/main/java/com/example/calendarandroid/DateActivity.java
+++ phase2/CalendarAndroid/app/src/main/java/com/example/calendarandroid/DateActivity.java
@@ -6,11 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class DateActivity extends AppCompatActivity {
 
@@ -18,6 +24,9 @@ public class DateActivity extends AppCompatActivity {
     LocalDateTime date;
     DateListAdopter eLAdapter;
     RecyclerView recyclerView;
+    TextView dateText;
+    TextView noEvents;
+    Format formatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +39,53 @@ public class DateActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         currentCalendar = MainActivity.currentCalendar;
+        dateText = (TextView) findViewById(R.id.this_date);
+        noEvents = (TextView) findViewById(R.id.empty_ls);
 
         // TODO: change this so date refers to whichever date was clicked on in MonthView
         date = LocalDateTime.now();
+        formatter = new SimpleDateFormat("MMM. dd", Locale.CANADA);
+        dateText.setText(formatter.format(date));
 
 
         List<Event> input = currentCalendar.search("any", date);
+        if (input.size() == 0) {
+            noEvents.setVisibility(View.VISIBLE);
+        } else {
+            noEvents.setVisibility(View.INVISIBLE);
+        }
         // define an adapter
         eLAdapter = new DateListAdopter(input);
         recyclerView.setAdapter(eLAdapter);
     }
 
-    public void nextDate() {
+    public void nextDate(View v) {
         // change the date to the next one
+        date = date.plusDays(1);
+        dateText.setText(formatter.format(date));
+        List<Event> input = currentCalendar.search("any", date);
+        if (input.size() == 0) {
+            noEvents.setVisibility(View.VISIBLE);
+        } else {
+            noEvents.setVisibility(View.INVISIBLE);
+        }
+        eLAdapter.updateList(input);
+    }
 
+    public void previousDate(View v) {
+        // change the date to the previous one
+        date = date.minusDays(1);
+        dateText.setText(formatter.format(date));
+        List<Event> input = currentCalendar.search("any", date);
+        if (input.size() == 0) {
+            noEvents.setVisibility(View.VISIBLE);
+        } else {
+            noEvents.setVisibility(View.INVISIBLE);
+        }
+        eLAdapter.updateList(input);
+    }
+
+    public void backToCal(View v) {
+        finish();
     }
 }
